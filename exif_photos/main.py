6bc48763e0f00234a9c5fcff3cba73dcf086447e
suregoth <suregoth@wp.pl>
2022-07-03 17:44:31 +0200

    added error handling

--- exif_photos/main.py
+++ exif_photos/main.py
@@ -4,14 +4,29 @@ from PIL import Image, ExifTags
 
 img_folder = input(r"Enter the file path (e.g.:\"C:\Users\sureg\Desktop\nauka\python\public\exif_photos\pics\"): ")
 img_contents = os.listdir(img_folder)
+count = 0
+
+def get_date():
+    exif = {ExifTags.TAGS[k]: v for k, v in pil_img._getexif().items() if k in ExifTags.TAGS}
+    global date
+    date = {exif[k] for k in exif if k == 'DateTimeOriginal'}
+
+def rename_file():
+    new_path = os.path.join(img_folder, datetime)
+    pil_img.close()
+    os.rename(full_path, new_path) 
 
 for image in img_contents:
     full_path = os.path.join(img_folder, image)
-#    ext = os.path.splitext(image)[1]
     pil_img = Image.open(full_path)
-    exif = {ExifTags.TAGS[k]: v for k, v in pil_img._getexif().items() if k in ExifTags.TAGS}
-    date = {exif[k] for k in exif if k == 'DateTimeOriginal'}
-    datetime = str(date).replace(":", "").replace(" ", "-").replace("{", "").replace("}", "").strip("'") + ".jpg"
-    new_path = os.path.join(img_folder, datetime)
-    pil_img.close()
-    os.rename(full_path, new_path)  
+    try:
+        get_date()
+        new_date = str(date).replace(":", "").replace(" ", "-").replace("{", "").replace("}", "").strip("'")
+        datetime = new_date + ".jpg" 
+        rename_file()
+    except FileExistsError:
+        count += 1
+        datetime = new_date + "-" + str(count) +".jpg"
+        rename_file()
+    except AttributeError:
+        continue
